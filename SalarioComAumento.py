ea5600a1a34b042482085ce3e1b9a9519b8b95ff
DanielPylu <ssidante001@gmail.com>
2026-01-19 12:50:10 -0300

    SalarioComAumento.py

--- SalarioComAumento.py
+++ SalarioComAumento.py
@@ -1,5 +1,4 @@
 sa = float(input('Digite o valor do seu salário: '))
 aumento = sa * 1.15 # Digite o valor do aumento no 1,15
-print('-' * 35)
+print('-' * 35) # efeito visual
 print('O seu salário com aumento vale {:.2f}'.format(aumento))
-
