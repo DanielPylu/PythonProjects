8deb308233b3450b982a1b54a55ad1b13f02b3b8
DanielPylu <ssidante001@gmail.com>
2026-01-19 12:33:39 -0300

    ProdutoComDesconto.py

--- ProdutoComDesconto.py
+++ ProdutoComDesconto.py
@@ -1,7 +1,7 @@
 p = float(input('Digite o valor do produto: '))
 d = float(input('Digite o valor do desconto: ')) # se você quer um desconto com ',', no int coloque float
-valorD = (p / 100) * d # p / 100 = 50, 50 * 5 = 250 - valor do desconto
-ValorP = (p - valorD) # sempre bote o maior valor primeiro para não dar negativo
+valorD = (p / 100) * d
+ValorP = (p - valorD)
 
 
 print('-' * 35) # efeito visual
