4f66bf9d16ef7d87b714aef304a1698ef82cc8e1
DanielPylu <ssidante001@gmail.com>
2026-01-19 12:16:28 -0300

    Add files via upload
    Você coloca os metros da parede e vai calcular que cada litro de tinta pinta 2m²

---
+++ LitrosDeTinta.py
@@ -0,0 +1,8 @@
+a = float(input('Digite a altura: '))
+l = float(input('Digite a largura: '))
+m = (a * l) / 2
+
+
+print('-' * 35) # efeito visual
+print('São necessários {:.1f} Litros de tinta!'.format(m)) # se você quer um código preciso apaga o ":.1f" dentros dos {}
+print('-' * 35) # efeito visual
