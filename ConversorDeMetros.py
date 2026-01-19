f09b66a60ffe5078b4e7f5e50792a7c59c480689
DanielPylu <ssidante001@gmail.com>
2026-01-18 22:33:55 -0300

    ConversorDeMetros.py

--- ConversorDeMetros.py
+++ ConversorDeMetros.py
@@ -1,4 +1,6 @@
 m = int(input('Valor em metros: '))
 c = (m * 100)
 mi = (m * 1000)
-print('-- O valor de centímetros: {}\n-- O valor de milímetros: {}'.format(c, mi))
+print('-- O valor de centímetros: {}\n-- O valor de milímetros: {}'.format(c, mi)) # você pode deixar assim para quebrar a linha no '\n" ou criar um novo print
+# para querer o print apage o "#" do inicio do print, e na linha de cima apage o \n ate as aspas, e apage o "mi" do .format e deixe so o c
+#print(-- O valor de milímetros: {}'.format(mi))
