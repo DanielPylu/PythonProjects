d33a1bb509eb47e4aa89ed0a7e61c4b4c93e6196
DanielPylu <ssidante001@gmail.com>
2026-01-18 22:21:26 -0300

    Add files via upload
    Você vê quantos dólares você pode pagar com reais na sua conta

---
+++ ReaisEmDolar.py
@@ -0,0 +1,3 @@
+r = float(input('Digite seu credito no banco em R$: '))
+d = (r / 5.37) # valor em dólar atualmente, mas pode ser alterado
+print('Você pode comprar {:.3f} em dólar.'.format(d)) # Use o ":.3f" para poder deixar 3 números depois da vírgula, mas pode ser alterado
