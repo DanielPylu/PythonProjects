95ef15dac775b97d2f95962ec4e362ef53d4569d
DanielPylu <ssidante001@gmail.com>
2026-01-19 12:17:49 -0300

    LitrosDeTinta.py

--- LitrosDeTinta.py
+++ LitrosDeTinta.py
@@ -1,8 +1,9 @@
 a = float(input('Digite a altura: '))
 l = float(input('Digite a largura: '))
-m = (a * l) / 2
+m = (a * l) / 2 # cada litro vai cubrir 2m², mas você pode deixar como você quiser
 
 
 print('-' * 35) # efeito visual
 print('São necessários {:.1f} Litros de tinta!'.format(m)) # se você quer um código preciso apaga o ":.1f" dentros dos {}
+
 print('-' * 35) # efeito visual
